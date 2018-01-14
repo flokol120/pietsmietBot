465fc5f5746a9b951f0fd041651ad2fc3b96fc12
Flo <flomax.doerr@web.de>
2018-01-14 23:44:10 +0100

    init commit

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const fs = require('fs');
 const key = require('./key');
 const client = new Discord.Client();
 
-//invite URL = https://discordapp.com/oauth2/authorize?&client_id=402225466108411936&scope=bot&permissions=0
+//invite URL
+var inviteURL = 'https://discordapp.com/oauth2/authorize?&client_id=402225466108411936&scope=bot&permissions=0'
 
 //on client ready. Sets the current game.
 
@@ -23,8 +24,14 @@ client.on('message', message => {
       //to get the proper index
       var random = Math.floor(Math.random() * (files.length - 1));
       //sends the gif into the channel where the command was executed
-      message.channel.send("", {file: './gifs/' + files[24]});
+      message.channel.send("", {file: './gifs/' + files[random]});
     });
+  }else if(message.content === '!invite' || message.content === '!einladen'){
+    message.channel.send('Hier ist der invite-link des Bots: ' + inviteURL);
+  }else if(message.content === '!help' || message.content === '!hilfe'){
+    message.channel.send('Folgende Commands stehen zur Verfügung:\n!pietsmiet'
+    + '  -  gibt dir eins der geilen GIFs der PietSmiet Truppe!\n!invite ODER !einladen'
+    + '  -  schickt dir denn invite link zu.\n!help ODER !hilfe  -  zeige diesen Text an.');
   }
 });
 
