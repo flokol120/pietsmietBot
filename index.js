c8cbf1d9b31ca26df65f75123fcc239810165fb6
flokol120 <flomax.doerr@web.de>
2018-01-15 15:06:53 +0100

    updated core command

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ client.on('ready', () => {
 //on message emmits
 
 client.on('message', message => {
-  //cpre command '!pietsmiet'
   var messageCommands = message.content.split(' ');
-  if (messageCommands[0] === '!pietsmiet') {
+  //core command '!pietsmiet' or '!ps'
+  if (messageCommands[0] === '!pietsmiet' || messageCommands[0] === '!ps') {
 	if(messageCommands[1] === 'gif'){
 		//reads all avalible gifs from the '/gifs/' folder
 		fs.readdir('./gifs/', function (err, files) {
@@ -31,11 +31,12 @@ client.on('message', message => {
 		  var random = Math.floor(Math.random() * (files.length - 1));
 		  //sends the gif into the channel where the command was executed
 		  message.channel.send("", {file: './gifs/' + files[random]});
+		  console.log('GIF was sent to ' + message.channel.name)
 		});
 	}else if(messageCommands[1] === 'invite' || messageCommands[1] === 'einladen'){
 		message.channel.send('Hier ist der invite-link des Bots: ' + inviteURL);
 	}else if(messageCommands[1] === 'help' || messageCommands[1] === 'hilfe'){
-    	message.channel.send('Folgende Commands stehen mit dem command ´!pietsmiet´ zur Verfügung:\n' +
+    	message.channel.send('Folgende Commands stehen mit dem command ´!pietsmiet´ oder ´!ps´ zur Verfügung:\n' +
 						 	 'gif  -  gibt dir eins der geilen GIFs der PietSmiet Truppe!\n' +
 						 	 'invite ODER einladen  -  schickt dir denn invite link zu.\n' +
 						 	 'help ODER hilfe  -  zeige diesen Text an.');
