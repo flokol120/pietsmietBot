117365eb5995fa25b4215d03967f805b5c4c2dc2
flokol120 <flomax.doerr@web.de>
2018-01-15 14:49:59 +0100

    added a core command outsourced clientID beautified help command output for 'Gambleon'

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const fs = require('fs');
 const key = require('./key');
 const client = new Discord.Client();
 
+var clientID = '402225466108411936';
+
 //invite URL
-var inviteURL = 'https://discordapp.com/oauth2/authorize?&client_id=402225466108411936&scope=bot&permissions=0'
+var inviteURL = 'https://discordapp.com/oauth2/authorize?&client_id=' + clientID +
+				'&scope=bot' + 
+				'&permissions=0';
 
 //on client ready. Sets the current game.
 
@@ -16,22 +20,26 @@ client.on('ready', () => {
 //on message emmits
 
 client.on('message', message => {
-  //command '!pietsmiet'
-  if (message.content === '!pietsmiet') {
-    //reads all avalible gifs from the '/gifs/' folder
-    fs.readdir('./gifs/', function (err, files) {
-      //generates a random number from 0 to the length of the avalible files - 1
-      //to get the proper index
-      var random = Math.floor(Math.random() * (files.length - 1));
-      //sends the gif into the channel where the command was executed
-      message.channel.send("", {file: './gifs/' + files[random]});
-    });
-  }else if(message.content === '!invite' || message.content === '!einladen'){
-    message.channel.send('Hier ist der invite-link des Bots: ' + inviteURL);
-  }else if(message.content === '!help' || message.content === '!hilfe'){
-    message.channel.send('Folgende Commands stehen zur Verfügung:\n!pietsmiet'
-    + '  -  gibt dir eins der geilen GIFs der PietSmiet Truppe!\n!invite ODER !einladen'
-    + '  -  schickt dir denn invite link zu.\n!help ODER !hilfe  -  zeige diesen Text an.');
+  //cpre command '!pietsmiet'
+  var messageCommands = message.content.split(' ');
+  if (messageCommands[0] === '!pietsmiet') {
+	if(messageCommands[1] === 'gif'){
+		//reads all avalible gifs from the '/gifs/' folder
+		fs.readdir('./gifs/', function (err, files) {
+		  //generates a random number from 0 to the length of the avalible files - 1
+		  //to get the proper index
+		  var random = Math.floor(Math.random() * (files.length - 1));
+		  //sends the gif into the channel where the command was executed
+		  message.channel.send("", {file: './gifs/' + files[random]});
+		});
+	}else if(messageCommands[1] === 'invite' || messageCommands[1] === 'einladen'){
+		message.channel.send('Hier ist der invite-link des Bots: ' + inviteURL);
+	}else if(messageCommands[1] === 'help' || messageCommands[1] === 'hilfe'){
+    	message.channel.send('Folgende Commands stehen zur Verfügung:\n' + 
+						 	 '!pietsmiet  -  gibt dir eins der geilen GIFs der PietSmiet Truppe!\n'
+						 	 '!invite ODER !einladen  -  schickt dir denn invite link zu.\n' + 
+						 	 '!help ODER !hilfe  -  zeige diesen Text an.');
+	}
   }
 });
 
